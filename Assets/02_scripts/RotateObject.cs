e3d6cb0c30375e132f4ca6716452c3e2d8d8785b
Adam Chernick <jchernick2010@gmail.com>
2020-08-07 22:28:21 -0400

    clean up
    clean up

---
+++ Assets/02_scripts/RotateObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    void Update()
+    {
+        this.transform.Rotate(this.gameObject.transform.rotation.x, this.gameObject.transform.rotation.y + 50f, this.gameObject.transform.rotation.z, Space.Self);
+    }
+}
